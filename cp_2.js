1c46d46c18832b41886992117c7bde4d0a1f8df0
Ingridtsp <ingridnicole@usf.edu>
2025-10-19 20:21:38 -0400

    committing step 4:async/await

--- cp_2.js
+++ cp_2.js
@@ -15,3 +15,15 @@ function fetchProductsThen() {
     })
     .catch(error => handleError(error));
 }
+
+// STEP 4: Using async/await
+async function fetchProductsAsync() {
+  try {
+    const response = await fetch('https://www.course-api.com/javascript-store-products');
+    if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+    const products = await response.json();
+    displayProducts(products);
+  } catch (error) {
+    handleError(error);
+  }
+}
